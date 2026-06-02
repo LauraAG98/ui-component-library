b9d1e749c8d3f8054536f379711dc87a9e0f7a18
LauraAG98 <lauraguirregarcia@outlook.com>
2026-06-02 17:32:44 -0500

    feat: Display the resource and status selectors on the main screen.

--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,12 +1,73 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
+import { ExplorerService, ResourceType } from './services/explorer';
+import { ButtonComponent, CardComponent, SelectComponent, TableComponent, SelectOption, TableAction, TableColumn } from 'ui-lib';
+import { Character } from './models/character.interface';
+import { Episode } from './models/episode.interface';
+import { Location } from './models/location.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, ButtonComponent, CardComponent, SelectComponent, TableComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
-  protected readonly title = signal('demo-app');
+  
+  /** Se inyecta el servicio */
+  private explorer = inject(ExplorerService);
+
+  /** Variables del servicio para que la tabla y los filtros se actualicen solos */
+  rows = this.explorer.rows;
+  loading = this.explorer.loading;
+  errorMessage = this.explorer.errorMessage;
+  activeResource = this.explorer.activeResource;
+
+  /** Registro para mostrar en el modal */
+  selectedRow = signal<Character | Episode | Location | null>(null);
+
+  /** Opciones para el select de recurso */
+  resourceOptions: SelectOption[] = [
+    {label: 'Characters', value: 'character'},
+    {label: 'Episodes', value: 'episode'},
+    {label: 'Locations', value: 'location'}
+  ];
+
+  /** Opciones para el filtro de status*/
+  statusOptions: SelectOption[] = [
+    {label: 'Alive', value: 'Alive'},
+    {label: 'Dead', value: 'Dead'},
+    {label: 'Unknown', value: 'unknown'}
+  ];
+
+  /** Cambia el recurso activo */
+  onResourceChange(option: SelectOption): void{
+    this.explorer.changeResource(option.value as ResourceType);
+  }
+
+  /** Cambia el filtro de status */
+  onStatusChange(option: SelectOption): void{
+    this.explorer.changeStatus(option.value);
+  }
+
+  /** Maneje las acciones de la tabla */
+  onActionTriggered(action: TableAction<Character | Episode | Location>): void {
+    if(action.action === 'view') {
+      this.selectedRow.set(action.row);
+    }
+  }
+
+  /**Columnas según el recurso que este activo */
+  columns: TableColumn[] = [
+    {key: 'name', header: 'Nombre'},
+    {key: 'status', header: 'Estado'},
+    {key: 'species', header: 'Especie'}
+  ]
+
+  /** Cierra el modal */
+  closeModal(): void {
+    this.selectedRow.set(null);
+  }
 }
