57f6f70725b3eaccb80b95200b201ab662915254
LauraAG98 <lauraguirregarcia@outlook.com>
2026-06-01 21:58:28 -0500

    chore: install and configure tailwind css v3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./projects/ui-lib/src/**/*.{html,ts}",
+    "./projects/demo-app/src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
